d7a34c0116874a6903a29211092c9b55ee338a59
Ubuntu <ops@TechOps-TA-Ubuntu.nhmlki515goelc3lwlbowkj2ud.fx.internal.cloudapp.net>
2020-02-14 16:18:09 +0000

    file permissions -> 755

---
+++ run-compose.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+# create grafana fs structure for data persistency
+create_fs_structure() {
+        #nexus
+        mkdir ./nexus-data
+        chown -R 200 nexus-data
+
+}
+
+# run compose
+run_compose() {
+
+        docker-compose up -d
+
+}
+
+#install docker in jenkins
+install_docker_in_jenkins() {
+
+        docker exec -it -u root jenkins bash -c '/var/jenkins_home/install-docker.sh'
+}
+
+create_fs_structure
+run_compose
+install_docker_in_jenkins
+
+exit 0
+
